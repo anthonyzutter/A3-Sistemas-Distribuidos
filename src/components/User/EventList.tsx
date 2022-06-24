5f1bbb1b793d415a3b11bd60dec42a94c24bac47
Anthony <60969070+anthonyzutter@users.noreply.github.com>
2022-06-23 23:51:00 -0300

    event list

--- src/components/User/EventList.tsx
+++ src/components/User/EventList.tsx
@@ -1,21 +1,43 @@
+import React, { useState, useEffect } from "react";
 import "../../static/EventList.css";
 import { Link } from "react-router-dom";
 import EventCard from "./EventCard";
-
+import EventDataService from "../../service/EventService";
 
 const EventList = () => {
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    retrieveQuadras();
+  }, []);
+
+  const retrieveQuadras = () => {
+    EventDataService.getAll()
+      .then(response => {
+        setEvents(response.data);
+        console.log(response.data);
+      })
+      .catch(e => {
+        console.log(e);
+      });
+  };
+
   return (
     <div className="events-container">
-      <EventCard/>
-      <EventCard/>
-      <EventCard/>
-      <EventCard/>
-      <EventCard/>
-      <EventCard/>
-      <EventCard/>
-      <EventCard/>
-      <EventCard/>
-      <EventCard/>
+      {events &&
+            events.map(event => (
+              <EventCard event={event}/>
+            ))}
+      
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
+      <EventCard />
 
     </div>
   )
